2430d8d941903829199ce8aa2c7ba74f82af4f49
Talal Ahmed (OSV) <talal.ahmed.osv@fedex.com>
2018-12-02 09:04:24 -0500

    remove regexp warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,8 +14,8 @@ public class Greeter {
   }
 
 /**
-* @param someone the name of the person 
-* @return greeting string 
+* @param someone the name of the person
+* @return greeting string
 */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -1,4 +1,4 @@
 /**
- *This is the package name
+ *This is the package name.
  */
 package com.example;
